fa5614c04e381f1326d072a4f4a4a58682cd8dd5
Rishabh@2500 <sharmarishabh0025@gmail.com>
2026-04-06 09:07:35 +0530

    PORT fixed

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const app = require('./src/app');
 const { connectDB } = require('./src/models');
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
